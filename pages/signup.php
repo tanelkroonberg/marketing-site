da5f5e2093ff8a6eb2a6f64d8aab3feef4b3dba6
tanelkroonberg <tanel.kroonberg@khk.ee>
2013-02-14 15:10:49 +0200

    1 commit

---
+++ pages/signup.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <title>
+        Signup
+    </title>
+    <style>
+        .styleform label {
+            float: left;
+        }
+
+        .styleform .clear {
+            clear: both;
+        }
+
+        .btn-success {
+            margin-left: 200px
+        }
+    </style>
+</head>
+<body>
+<div class="styleform">
+    <form>
+        <label>Eesnimi:</label><input type="text" name="firstname"><br>
+        <label>Perenimi:</label><input type="text" name="lastname"><br>
+        <label>Email:</label><input type="text" name="email"><br>
+        <button class="btn btn-success">Esita</button>
+
+    </form>
+</div>
+</body>
+</html>
